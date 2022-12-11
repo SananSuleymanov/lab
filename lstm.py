3418ce697e697370d0717a02b02bf81d0e3c44a8
sasule <sasule@ttu.ee>
2022-12-11 19:10:46 +0200

    new commit

--- lstm.py
+++ lstm.py
@@ -1,25 +1,24 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
+from keras.layers import Dense, LSTM, Dropout
+from keras.models import Sequential
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
-"""#Read Dataset"""
+#Read Dataset
 
-data= pd.read_csv('IBM.csv')
+data = pd.read_csv('IBM.csv')
 
 data.head()
 
-"""#Data Preprocessing
-The close value of the stock is chosen as the data to train our model. Data is scaled using MinMaxScaler. The dataset consist of 755 datapoints which is divided to train and test datasets. First 600 is selected as train dataset and other 155 datapoinst as test dataset.
-"""
+#Data Preprocessing
+#The close value of the stock is chosen as the data to train our model. Data is scaled using MinMaxScaler. The dataset consist of 755 datapoints which is divided to train and test datasets. First 600 is selected as train dataset and other 155 datapoinst as test dataset.
 
-data['Date']= pd.to_datetime(data.Date)
+data['Date'] = pd.to_datetime(data.Date)
 
 feature = data.iloc[:, 4:5]
 
-scale= MinMaxScaler(feature_range=(0, 1))
+scale = MinMaxScaler(feature_range=(0, 1))
 
 feature = scale.fit_transform(feature)
 
@@ -28,81 +27,80 @@ data.info()
 train = feature[:600]
 test = feature[600:]
 
-"""The previous 30 days data will be used as an input for the model to predict the value of next day. That's why the training data is divided to the two parts using this script. Furthermore, for the model we need to have 3D data and because of that it is reshaped."""
+#The previous 30 days data will be used as an input for the model to predict the value of next day. That's why the training data is divided to the two parts using this script. Furthermore, for the model we need to have 3D data and because of that it is reshaped.
 
-train_x=[]
-train_y=[]
+trainx = []
+trainy = []
 
 for i in range(30, 600):
-  train_x.append(train[i-30:i, 0])
-  train_y.append(train[i, 0])
+    trainx.append(train[i-30:i, 0])
+    trainy.append(train[i, 0])
 
-train_x = np.array(train_x)
-train_y = np.array(train_y)
+train_x = np.array(trainx)
+train_y = np.array(trainy)
 
-train_x.shape
 
 train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], 1))
 
 
-"""
-The test dataset is also preprocessed as training dataset for testing the performance of the model
-"""
 
-test_x=[]
-test_y=[]
+#The test dataset is also preprocessed as training dataset for testing the performance of the model
+
+testx = []
+testy = []
 
 for i in range(30, 155):
-  test_x.append(test[i-30:i, 0])
-  test_y.append(test[i, 0])
+    testx.append(test[i-30:i, 0])
+    testy.append(test[i, 0])
 
-test_x = np.array(test_x)
-test_y = np.array(test_y)
+test_x = np.array(testx)
+test_y = np.array(testy)
 
 test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], 1))
 
 if __name__ == '__main__':
-  """#Model Development"""
-  model = tf.keras.models.Sequential()
-  
-  model.add(tf.keras.models.LSTM (units=100, return_sequences= True, input_shape = (train_x.shape[1], 1)))
-  model.add(tf.keras.models.Dropout(rate=0.2))
-  
-  model.add(tf.keras.models.LSTM (units=100, return_sequences= True))
-  model.add(tf.keras.models.Dropout(rate=0.2))
-  
-  model.add(tf.keras.models.LSTM (units=100, return_sequences= True))
-  model.add(tf.keras.models.Dropout(rate=0.2))
-  
-  model.add(tf.keras.models.LSTM (units=100, return_sequences= True))
-  model.add(tf.keras.models.Dropout(rate=0.2))
-  
-  model.add(tf.keras.models.LSTM (units=100))
-  model.add(tf.keras.models.Dropout(rate=0.2))
-  
-  model.add(tf.keras.models.Dense(units=1))
-  
-  model.compile(loss='mean_squared_error', optimizer='adam')
-  model.summary()
-  
-  """#Model Training """
-  
-  history= model.fit(train_x, train_y, epochs=100, batch_size=16)
-  loss = history.history['loss']
-  
-  plt.plot(loss)
+    #Model Development
+    model = Sequential()
 
-  """ Prediction """
-  predict_y = model.predict(test_x)
+    model.add(LSTM(units=100, return_sequences=True,
+              input_shape=(train_x.shape[1], 1)))
+    model.add(Dropout(rate=0.2))
 
-  predict_y =np.reshape(predict_y, (predict_y.shape[0], predict_y.shape[1]))
-  test_y =np.reshape(test_y, (test_y.shape[0], 1))
+    model.add(LSTM(units=100, return_sequences=True))
+    model.add(Dropout(rate=0.2))
 
-  """The result is transformed to initial form of dataset for getting appropriate value."""
+    model.add(LSTM(units=100, return_sequences=True))
+    model.add(Dropout(rate=0.2))
 
-  predict_y = scale.inverse_transform(predict_y)
-  test_y = scale.inverse_transform(test_y)
+    model.add(LSTM(units=100, return_sequences=True))
+    model.add(Dropout(rate=0.2))
 
-  plt.plot(predict_y, color='red')
-  plt.plot(test_y, color='blue')
-  plt.show()
+    model.add(LSTM(units=100))
+    model.add(Dropout(rate=0.2))
+
+    model.add(Dense(units=1))
+
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+
+    #Model Training
+
+    history = model.fit(train_x, train_y, epochs=100, batch_size=16)
+    loss = history.history['loss']
+
+    plt.plot(loss)
+
+    #Prediction 
+    predict_y = model.predict(test_x)
+
+    predict_y = np.reshape(predict_y, (predict_y.shape[0], predict_y.shape[1]))
+    test_y = np.reshape(test_y, (test_y.shape[0], 1))
+
+    #The result is transformed to initial form of dataset for getting appropriate value.
+
+    predict_y = scale.inverse_transform(predict_y)
+    test_y = scale.inverse_transform(test_y)
+
+    plt.plot(predict_y, color='red')
+    plt.plot(test_y, color='blue')
+    plt.show()
